8a25cb62024c2d97f05fac525811256f3d0c56cf
coulange severine <coulsev@gmail.com>
2022-06-08 15:01:40 +0200

    express10

--- models/user.js
+++ models/user.js
@@ -1,25 +1,26 @@
-const connection = require('../db-config');
-const Joi = require('joi');
-const argon2 = require('argon2');
+const connection = require("../db-config");
+const Joi = require("joi");
+const argon2 = require("argon2");
 
 const db = connection.promise();
 
 const validate = (data, forCreation = true) => {
-  const presence = forCreation ? 'required' : 'optional';
+  const presence = forCreation ? "required" : "optional";
   return Joi.object({
     email: Joi.string().email().max(255).presence(presence),
     firstname: Joi.string().max(255).presence(presence),
     lastname: Joi.string().max(255).presence(presence),
-    city: Joi.string().allow(null, '').max(255),
-    language: Joi.string().allow(null, '').max(255),
+    city: Joi.string().allow(null, "").max(255),
+    language: Joi.string().allow(null, "").max(255),
+    password: Joi.string().min(8).max(32),
   }).validate(data, { abortEarly: false }).error;
 };
 
 const findMany = ({ filters: { language } }) => {
-  let sql = 'SELECT * FROM users';
+  let sql = "SELECT * FROM users";
   const sqlValues = [];
   if (language) {
-    sql += ' WHERE language = ?';
+    sql += " WHERE language = ?";
     sqlValues.push(language);
   }
 
@@ -28,36 +29,50 @@ const findMany = ({ filters: { language } }) => {
 
 const findOne = (id) => {
   return db
-    .query('SELECT * FROM users WHERE id = ?', [id])
+    .query(
+      "SELECT id, email, firstname, lastname, city, language FROM users WHERE id = ?",
+      [id]
+    )
     .then(([results]) => results[0]);
 };
 
 const findByEmail = (email) => {
   return db
-    .query('SELECT * FROM users WHERE email = ?', [email])
+    .query("SELECT * FROM users WHERE email = ?", [email])
     .then(([results]) => results[0]);
 };
 
 const findByEmailWithDifferentId = (email, id) => {
   return db
-    .query('SELECT * FROM users WHERE email = ? AND id <> ?', [email, id])
+    .query("SELECT * FROM users WHERE email = ? AND id <> ?", [email, id])
     .then(([results]) => results[0]);
 };
 
-const create = (data) => {
-  return db.query('INSERT INTO users SET ?', data).then(([result]) => {
-    const id = result.insertId;
-    return { ...data, id };
+const create = ({ firstname, lastname, city, language, email, password }) => {
+  return hashPassword(password).then((hashedPassword) => {
+    return db
+      .query("INSERT INTO users SET ?", {
+        firstname,
+        lastname,
+        city,
+        language,
+        email,
+        hashedPassword,
+      })
+      .then(([result]) => {
+        const id = result.insertId;
+        return { firstname, lastname, city, language, email, id };
+      });
   });
 };
 
 const update = (id, newAttributes) => {
-  return db.query('UPDATE users SET ? WHERE id = ?', [newAttributes, id]);
+  return db.query("UPDATE users SET ? WHERE id = ?", [newAttributes, id]);
 };
 
 const destroy = (id) => {
   return db
-    .query('DELETE FROM users WHERE id = ?', [id])
+    .query("DELETE FROM users WHERE id = ?", [id])
     .then(([result]) => result.affectedRows !== 0);
 };
 
